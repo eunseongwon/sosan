76923ab1768986116c478050eb93541ad620a130
dbtmd635 <dbtmd635@gmail.com>
2026-03-27 16:31:05 +0900

    프로젝트 전체 환경(백엔드/프론트) 최초 업로드

---
+++ sosang-workspace/src/main/java/com/example/sosangworkspace/SosangWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sosangworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosangWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosangWorkspaceApplication.class, args);
+    }
+
+}
---
+++ sosang-workspace/src/test/java/com/example/sosangworkspace/SosangWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sosangworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosangWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
